9e9e8430e25d3a9dacc89137a45c9fafb0038581
coral <jonas@jonasbengtson.se>
2021-02-07 10:15:40 -0800

    ugh

--- src/engines/engine.rs
+++ src/engines/engine.rs
@@ -1,10 +1,5 @@
 use anyhow::Result;
 
-#[derive(Debug)]
-pub struct Pattern {
-    pub name: String,
-}
-
 pub trait Engine {
     fn bootstrap(&mut self) -> anyhow::Result<()>;
     fn list(&mut self) -> Vec<Pattern>;
---
+++ src/engines/pattern.rs
@@ -0,0 +1,4 @@
+//#[derive(Debug)]
+pub trait Pattern {
+    fn name() -> &str;
+}
