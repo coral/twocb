87f0482bee6a6ad63306bb91b798e0dc858e09c6
coral <jonas@jonasbengtson.se>
2021-02-22 21:50:09 -0800

    subtract

--- src/layers/blending.rs
+++ src/layers/blending.rs
@@ -2,9 +2,11 @@ use vecmath;
 
 mod add;
 mod screen;
+mod subtract;
 
 pub enum BlendModes {
     Add,
+    Subtract,
     Screen,
 }
 
@@ -17,6 +19,9 @@ pub fn blend(
     match mode {
         BlendModes::Add => {
             return add::add(op1, op2);
+        }
+        BlendModes::Subtract => {
+            return subtract::sub(op1, op2);
         }
         BlendModes::Screen => {
             return screen::screen(op1, op2);
---
+++ src/layers/blending/subtract.rs
@@ -0,0 +1,36 @@
+pub fn sub(
+    op1: Vec<vecmath::Vector3<f64>>,
+    mut op2: Vec<vecmath::Vector3<f64>>,
+) -> Vec<vecmath::Vector3<f64>> {
+    for (i, v) in op1.iter().enumerate() {
+        op2[i] = vecmath::vec3_sub(op1[i], op2[i]);
+        op2[i][0] = op2[i][0].clamp(0.0, 1.0);
+        op2[i][1] = op2[i][1].clamp(0.0, 1.0);
+        op2[i][2] = op2[i][2].clamp(0.0, 1.0);
+    }
+    return op2;
+}
+
+////////////
+/// TESTS
+/////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn blending_sub() {
+        let op1 = vec![[1.0, 1.0, 1.0]];
+        let op2 = vec![[0.5, 0.5, 0.5]];
+        let m = sub(op1, op2);
+        assert!(m[0][0] == 0.5);
+        assert!(m[0][1] == 0.5);
+        assert!(m[0][2] == 0.5);
+
+        let op1 = vec![[0.0, 0.8, 1.3]];
+        let op2 = vec![[0.0, 0.4, 0.5]];
+        let m = sub(op1, op2);
+        assert!(m[0][0] == 0.0);
+        assert!(m[0][1] == 0.4);
+    }
+}
